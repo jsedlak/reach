994e98952326c7c97887433c00b09c1f71cc13b4
John Sedlak <john.sedlak@outlook.com>
2024-11-02 09:35:33 -0400

    Adds silo project Reorganizes projects in solution

--- src-app/Reach.AppHost/Program.cs
+++ src-app/Reach.AppHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Reach_AppApi>("reach-appapi");
+
+builder.AddProject<Projects.Reach_SiloHost>("reach-silohost");
+
 builder.Build().Run();
