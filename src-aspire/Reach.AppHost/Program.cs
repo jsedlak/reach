67d4caa66fb507e18d13e602f226fc8774473ab4
John Sedlak <john.sedlak@outlook.com>
2024-11-16 04:21:46 -0500

    Upgrades to .NET9

--- src-aspire/Reach.AppHost/Program.cs
+++ src-aspire/Reach.AppHost/Program.cs
@@ -3,18 +3,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 /* Add Our IDP */
 // TODO: Configure IDP as an aspire resource
 
-/* Adds Streaming Capabilities */
-var eventHubs = builder.AddAzureEventHubs("reach-event-hubs")
-    .RunAsEmulator()
-    .AddEventHub("ReachEvents");
+///* Adds Streaming Capabilities */
+//var eventHubs = builder.AddAzureEventHubs("reach-event-hubs")
+//    .RunAsEmulator()
+//    .AddEventHub("ReachEvents");
 
-var eventHubConnectionString = eventHubs.PublishAsConnectionString();
+//var eventHubConnectionString = eventHubs.PublishAsConnectionString();
 
 /* Add Our Mongo Storage */
 var mongo = builder.AddMongoDB("reach-mongo", 52296);
 
 /* Our Orleans Cluster & API */
-var storage = builder.AddAzureStorage("reach-cluster-storage").RunAsEmulator();
+var storage = builder.AddAzureStorage("reach-cluster-storage")
+    .RunAsEmulator(r => r.WithImage("azure-storage/azurite", "3.33.0"));
 var grainStorage = storage.AddBlobs("grain-state");
 var streamingStorage = storage.AddTables("streaming");
 var cluster = storage.AddTables("clustering");
