6bdd6983a6cf456be4877c283d881b5b95ec92e7
John Sedlak <john.sedlak@outlook.com>
2024-12-27 09:24:08 -0500

    Adds component event applies

--- src-cluster/Reach.Silo.Core/Content/Model/Component.cs
+++ src-cluster/Reach.Silo.Core/Content/Model/Component.cs
@@ -1,4 +1,6 @@
-﻿namespace Reach.Content.Model;
+﻿using Reach.Content.Events.Components;
+
+namespace Reach.Content.Model;
 
 public class Component
 {
@@ -7,4 +9,30 @@ public class Component
     public Guid DefinitionId { get; set; } = new Guid();
 
     public ComponentDefinition Definition { get; set; } = new();
+
+    public string Name { get; set; } = null!;
+
+    public string Slug { get; set; } = null!;
+
+    public void Apply(ComponentCreatedEvent @event)
+    {
+        Name = @event.Name;
+        Slug = @event.Slug;
+    }
+
+    public void Apply(ComponentDeletedEvent @event)
+    {
+        
+    }
+
+    public void Apply(ComponentRenamedEvent @event)
+    {
+        Name = @event.Name;
+        Slug = @event.Slug;
+    }
+
+    public void Apply(ComponentFieldValueSetEvent @event)
+    {
+        
+    }
 }
