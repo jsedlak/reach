d812578942ba12b0622fa2c398d36006b4fbd219
John Sedlak <john.sedlak@outlook.com>
2025-01-01 09:13:16 -0500

    Fixes component unit test

--- src-cluster/Reach.Silo.Core/Content/Model/ComponentDefinition.cs
+++ src-cluster/Reach.Silo.Core/Content/Model/ComponentDefinition.cs
@@ -1,15 +1,66 @@
-﻿namespace Reach.Content.Model;
+﻿using Reach.Content.Events.ComponentDefinitions;
+
+namespace Reach.Content.Model;
 
 /// <summary>
 /// Describes a template for a <see cref="Component"/>
 /// </summary>
 public class ComponentDefinition
 {
-    public Guid Id { get; set; } = new Guid();
+    public void Apply(ComponentDefinitionCreatedEvent @event)
+    {
+        OrganizationId = @event.OrganizationId;
+        HubId = @event.HubId;
+        Id = @event.AggregateId;
+        Name = @event.Name;
+        Slug = @event.Slug;
+        
+    }
 
-    public string Name { get; set; } = "Untitled Component";
+    public void Apply(ComponentDefinitionRenamedEvent @event)
+    {
+        Name = @event.Name;
+        Slug = @event.Slug;
+    }
+
+    public void Apply(ComponentDefinitionDeletedEvent @event)
+    {
+        IsDeleted = true;
+    }
+
+    public void Apply(FieldAddedToComponentDefinition @event)
+    {
+        Fields =
+        [
+            ..Fields,
+            @event.Field
+        ];
+    }
+
+    public void Apply(FieldRemovedFromComponentDefinitionEvent @event)
+    {
+        Fields = Fields.Where(m => m.Id != @event.FieldId).ToArray();
+    }
+    
+    /// <summary>
+    /// Gets or Sets the identifier of the organization to which this definition belongs
+    /// </summary>
+    public Guid OrganizationId { get; set; }
+
+    /// <summary>
+    /// Gets or Sets the identifier of the hub to which this definition belongs
+    /// </summary>
+    public Guid HubId { get; set; }
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Slug { get; set; } = null!;
 
     public Guid ParentId { get; set; } = Guid.Empty;
 
-    public FieldDefinition[] Fields { get; set; } = Array.Empty<FieldDefinition>();
+    public Field[] Fields { get; set; } = Array.Empty<Field>();
+
+    public bool IsDeleted { get; set; }
 }
--- src-lib/Reach.Core/Content/Model/Field.cs
+++ src-lib/Reach.Core/Content/Model/Field.cs
@@ -4,21 +4,25 @@
 /// Represents an instance of a <see cref="FieldDefinition"/> combining
 /// parameters and data with an underlying definition
 /// </summary>
+[GenerateSerializer]
 public class Field
 {
     /// <summary>
     /// Gets or Sets the unique identifier
     /// </summary>
+    [Id(0)]
     public Guid Id { get; set; } = Guid.NewGuid();
 
     /// <summary>
     /// Gets or Sets the underlying definition
     /// </summary>
+    [Id(1)]
     public Guid DefinitionId { get; set; }
 
     /// <summary>
     /// Gets or Sets the value being stored by this instance
     /// </summary>
+    [Id(2)]
     public string Value { get; set; } = string.Empty;
 }
 
