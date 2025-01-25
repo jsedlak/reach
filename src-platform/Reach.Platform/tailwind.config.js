b4ac09bde1110f330e14eb9a8b881db5550d38fd
John Sedlak <john.sedlak@outlook.com>
2025-01-25 10:35:53 -0500

    Adds Tailwind CLI, updates Tazor to fix styles

---
+++ src-platform/Reach.Platform/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "../Reach.Platform.Components/*.cshtml",
+        "../Reach.Platform.Components/*.css",
+        "../Reach.Platform.Editors/*.cshtml",
+        "../Reach.Platform.Editors/*.css"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
