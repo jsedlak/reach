45fecb9569cf00402d6bad20acb22a23b73b3003
John Sedlak <john.sedlak@outlook.com>
2025-02-04 23:55:31 -0500

    Preps view grains for direct comms

--- src-cluster/Reach.Silo.Core/Content/GrainModel/IComponentDefinitionViewGrain.cs
+++ src-cluster/Reach.Silo.Core/Content/GrainModel/IComponentDefinitionViewGrain.cs
@@ -1,6 +1,18 @@
+using Reach.Content.Events.ComponentDefinitions;
+
 namespace Reach.Silo.Content.GrainModel;
 
 public interface IComponentDefinitionViewGrain : IGrainWithStringKey
 {
-    
+    Task Handle(ComponentDefinitionCreatedEvent @event);
+
+    Task Handle(ComponentDefinitionRenamedEvent @event);
+
+    Task Handle(ComponentDefinitionRendererSetEvent @event);
+
+    Task Handle(FieldAddedToComponentDefinition @event);
+
+    Task Handle(FieldRemovedFromComponentDefinitionEvent @event);
+
+    Task Handle(ComponentDefinitionDeletedEvent @event);
 }
--- src-cluster/Reach.Silo.Core/Content/GrainModel/IComponentViewGrain.cs
+++ src-cluster/Reach.Silo.Core/Content/GrainModel/IComponentViewGrain.cs
@@ -1,6 +1,14 @@
+using Reach.Content.Events.Components;
+
 namespace Reach.Silo.Content.GrainModel;
 
 public interface IComponentViewGrain : IGrainWithStringKey
 {
-    
+    Task Handle(ComponentCreatedEvent @event);
+
+    Task Handle(ComponentRenamedEvent @event);
+
+    Task Handle(ComponentFieldValueSetEvent @event);
+
+    Task Handle(ComponentDeletedEvent @event);
 }
