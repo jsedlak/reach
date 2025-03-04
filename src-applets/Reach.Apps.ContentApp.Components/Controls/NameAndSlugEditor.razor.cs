203019dfe3f6afc056fc1d7d7603e4046a2ffebe
John Sedlak <john.sedlak@outlook.com>
2025-03-03 20:35:39 -0500

    Resolves #28 Updates name/slug editor

--- src-applets/Reach.Apps.ContentApp.Components/Controls/NameAndSlugEditor.razor.cs
+++ src-applets/Reach.Apps.ContentApp.Components/Controls/NameAndSlugEditor.razor.cs
@@ -8,8 +8,24 @@ namespace Reach.Apps.ContentApp.Components.Controls;
 /// </summary>
 public partial class NameAndSlugEditor : ComponentBase
 {
+    private bool _isSlugEditorVisible = false;
+    private string _slugCancelValue = "";
+    private bool _isValidating = false;
+    private Func<string, string, Task<bool>> _validationCallback;
+
+    public NameAndSlugEditor()
+    {
+        _validationCallback = (__name, __slug) => Task.FromResult(
+            !string.IsNullOrWhiteSpace(__name) && !string.IsNullOrWhiteSpace(__slug)
+        );
+    }
+
     private async Task OnNameChanged(string value)
     {
+        var areNameAndSlugAttached = 
+            (string.IsNullOrWhiteSpace(Name) && string.IsNullOrWhiteSpace(Slug)) ||
+            Name.ToSlug().Equals(Slug, StringComparison.OrdinalIgnoreCase);
+
         Name = value;
 
         if (NameChanged.HasDelegate)
@@ -17,14 +33,80 @@ public partial class NameAndSlugEditor : ComponentBase
             await NameChanged.InvokeAsync(Name);
         }
 
-        Slug = value.ToSlug();
+        if (areNameAndSlugAttached)
+        {
+            Slug = value.ToSlug();
 
-        if (SlugChanged.HasDelegate)
+            if (SlugChanged.HasDelegate)
+            {
+                await SlugChanged.InvokeAsync(Slug);
+            }
+        }
+
+        await ValidateData();
+    }
+
+    private async Task ValidateData()
+    {
+        _isValidating = true;
+        StateHasChanged();
+
+        var newValidValue = await Validate(Name, Slug);
+        IsValid = newValidValue;
+
+        if(IsValidChanged.HasDelegate)
+        {
+            await IsValidChanged.InvokeAsync(IsValid);
+        }
+
+        _isValidating = false;
+        StateHasChanged();
+    }
+
+    #region Slug Editing
+    private async Task OnSlugChanged(string newSlug)
+    {
+        Slug = newSlug.ToSlug();
+
+        if(SlugChanged.HasDelegate)
         {
             await SlugChanged.InvokeAsync(Slug);
         }
+
+        await ValidateData();
     }
-    
+
+    protected virtual async Task OnCancelSlugEdit()
+    {
+        Slug = _slugCancelValue;
+
+        if(SlugChanged.HasDelegate)
+        {
+            await SlugChanged.InvokeAsync(Slug);
+        }
+
+        _isSlugEditorVisible = false;
+        StateHasChanged();
+
+        await ValidateData();
+    }
+
+    protected virtual async Task OnConfirmSlugEdit()
+    {
+        _isSlugEditorVisible = false;
+        StateHasChanged();
+
+        await ValidateData();
+    }
+
+    protected virtual void OnBeginEditSlug()
+    {
+        _slugCancelValue = Slug;
+        _isSlugEditorVisible = true;
+        StateHasChanged();
+    }
+    #endregion
+
     [Parameter] public string Name { get; set; } = null!;
     
     [Parameter] public EventCallback<string> NameChanged { get; set; }
@@ -32,4 +114,15 @@ public partial class NameAndSlugEditor : ComponentBase
     [Parameter] public string Slug { get; set; } = null!;
     
     [Parameter] public EventCallback<string> SlugChanged { get; set; }
+
+    [Parameter] public bool IsValid { get; set; }
+
+    [Parameter]public EventCallback<bool> IsValidChanged { get; set; }
+
+    [Parameter]
+    public Func<string, string, Task<bool>> Validate
+    {
+        get { return _validationCallback; }
+        set { _validationCallback = value; }
+    }
 }
