3fab08e509676108497bb9a7571f28b4a678e830
John Sedlak <john.sedlak@outlook.com>
2025-03-07 19:36:29 -0500

    Style updates

--- src-platform/Reach.Platform.Components/Data/ShortGuid.razor.cs
+++ src-platform/Reach.Platform.Components/Data/ShortGuid.razor.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Reach.Platform.Components.Data;
 
 public partial class ShortGuid : ComponentBase
 {
+    private readonly IJSRuntime _jsRuntime;
+
+    public ShortGuid(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
     private async Task OnValueClicked()
     {
         if (Clicked.HasDelegate)
         {
             await Clicked.InvokeAsync();
         }
+    }
+
+    private async Task OnCopyClicked()
+    {
+        await _jsRuntime.InvokeVoidAsync("copyToClipboard", Value.ToString());
     }
     
     [Parameter]
