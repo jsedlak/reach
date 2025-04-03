acb6838f88ae46d444e07e8a8dcb7316a91d66a8
John Sedlak <john.sedlak@outlook.com>
2025-04-02 20:46:21 -0400

    Resolves #51 adds notification for copying guids

--- src-platform/Reach.Platform.Components/Data/ShortGuid.razor.cs
+++ src-platform/Reach.Platform.Components/Data/ShortGuid.razor.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Tazor.Components.App;
+using Tazor.Components.Content;
 
 namespace Reach.Platform.Components.Data;
 
 public partial class ShortGuid : ComponentBase
 {
     private readonly IJSRuntime _jsRuntime;
+    private readonly INotificationProvider _notificationProvider;
 
-    public ShortGuid(IJSRuntime jsRuntime)
+    public ShortGuid(IJSRuntime jsRuntime, INotificationProvider notificationProvider)
     {
+        _notificationProvider = notificationProvider;
         _jsRuntime = jsRuntime;
     }
 
@@ -23,8 +27,16 @@ public partial class ShortGuid : ComponentBase
     private async Task OnCopyClicked()
     {
         await _jsRuntime.InvokeVoidAsync("copyToClipboard", Value.ToString());
+
+        _notificationProvider.Add(new NotificationItem
+        {
+            Type = NotificationType.Success,
+            Title = "Copied!",
+            Message = $"Copied {Value.ToString()} to clipboard",
+            Icon = HeroIcons.ClipboardDocumentCheck()
+        });
     }
-    
+
     [Parameter]
     public Guid? Value { get; set; }
     
